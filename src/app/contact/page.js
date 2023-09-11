1b0ce4fe0358904011614937cb1d3802ee5aa9ab
Julian <cusco2891@gmail.com>
2023-09-10 23:52:50 -0400

    next template

---
+++ src/app/contact/page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function page() {
+
+
+    return (
+        <div>
+
+            <header>Contact</header>
+
+            <p>contact page</p>
+
+
+
+
+
+
+
+        </div>
+    )
+}
